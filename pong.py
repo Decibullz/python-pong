390317809ecd7c43bc3748e4b136595f4004c164
Cody Snell <codyjsnell@yahoo.com>
2021-01-08 01:49:25 -0600

    adds dynamic score

--- pong.py
+++ pong.py
@@ -124,10 +124,16 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0,0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center",font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0,0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center",font=("Courier", 24, "normal"))
 
 
     # paddle and ball collision
