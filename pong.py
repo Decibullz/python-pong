6f6e6f51b24bcad24cd9dd61f675985e72ac4af9
Cody Snell <codyjsnell@yahoo.com>
2021-01-08 01:13:38 -0600

    creates paddle a move down and paddle b up and down movement

--- pong.py
+++ pong.py
@@ -53,11 +53,30 @@ def paddle_a_up():
     y += 20
     paddle_a.sety(y)
 
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
 #Keyboard binding
 # tells window to listen for keyboard input
 wn.listen()
 # when user presses w, call on function
 wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+# for arrow keys use capital Up Down keywords
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 
 
